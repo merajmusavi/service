039b1dbe904cc722b9a7291eb5804c189afee8c0
merajuni7ed <merajmusavi1243m@gmail.com>
2023-03-01 21:52:42 +0330

    UPDATE

---
+++ app/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+return null;
+    }
+}
