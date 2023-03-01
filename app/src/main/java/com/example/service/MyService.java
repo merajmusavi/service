e9f9fbdc8a304409e729b2de46ecf7f22afd5973
merajuni7ed <merajmusavi1243m@gmail.com>
2023-03-01 22:01:28 +0330

    UPDATE

--- app/src/main/java/com/example/service/MyService.java
+++ app/src/main/java/com/example/service/MyService.java
@@ -1,8 +1,13 @@
 package com.example.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
     public MyService() {
@@ -15,17 +20,34 @@ public class MyService extends Service {
 
     @Override
     public void onDestroy() {
+        stopForeground(true);
         super.onDestroy();
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+startForeground(1000,createNot());
         return START_NOT_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
 return null;
+    }
+
+    public Notification createNot(){
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (notificationManager==null)
+            return null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("app","app",NotificationManager.IMPORTANCE_HIGH);
+        }
+
+        return new NotificationCompat.Builder(this,"app")
+                .setContentText("dfsgrege")
+                .build();
+
     }
 }
